a1a8bdb09f5ba9486774a1ac6df82cef63d145e3
Md. Atiour Islam <atiour.islam@gmail.com>
2017-06-20 02:21:08 +0600

    dd

---
+++ Jamiahsalafiyah.Web/Models/CompanyClass.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Jamiahsalafiyah.Web.Models
+{
+    public class CompanyClass
+    {
+        public System.Guid CompanyKey { get; set; }
+        [Display(Name = "ID")]
+        public string CompanyID { get; set; }
+        [Display(Name = "Company Name*")]
+        [Required(ErrorMessage = "Company Name is required")]
+        public string CompanyName { get; set; }
+        [Display(Name = "Address")]
+        public string CompanyAddress { get; set; }
+        [Display(Name = "Telephone")]
+        [Required(ErrorMessage = "Telephone Number is required")]
+        public string CompanyPhone { get; set; }
+        [Display(Name = "Alternate Contact #")]
+        public string CompanyMobile { get; set; }
+        [Display(Name = "Email")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "E-mail is not valid")]
+        public string CompanyEmail { get; set; }
+        [Display(Name = "Website")]
+        public string CompanyWebsite { get; set; }
+        [Display(Name = "Fax")]
+        public string CompanyFax { get; set; }
+        [Display(Name = "Represented By")]
+        public string ContactPersonName { get; set; }
+        [Display(Name = "Telephone")]
+        public string ContactPersonNo { get; set; }
+        public byte[] Logo { get; set; }
+        public string LogoType { get; set; }
+        public Nullable<bool> IsDelete { get; set; }
+        [Required(ErrorMessage = "The State name is required")]
+        public Nullable<int> StateCode { get; set; }
+        [Required(ErrorMessage = "The City Name is required")]
+        public Nullable<int> CityKey { get; set; }
+        [Required(ErrorMessage = "The Zipcode is required")]
+        public Nullable<long> ZIPKey { get; set; }
+        [Display(Name = "Title")]
+        public string Title { get; set; }
+        [Display(Name = "Email")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "E-mail is not valid")]
+        public string ContactEmail { get; set; }
+
+        //public virtual CityList CityList { get; set; }
+        //public virtual StateList StateList { get; set; }
+        //public virtual ZIPList ZIPList { get; set; }
+    }
+
+    public class tempForm
+    {
+
+        public List<Forms> Formlist { get; set; }
+
+
+    }
+    public class tempDocType
+    {
+        public System.Guid ID { get; set; }
+        public string TName { get; set; }
+        public string ColorCode { get; set; }
+        public Nullable<bool> IsDelete { get; set; }
+        public Nullable<int> DocumentForID { get; set; }
+
+        //public virtual DocumentFor DocumentFor { get; set; }
+    }
+    public class StaffClass
+    {
+        public System.Guid PersonnelKey { get; set; }
+        [Display(Name = "ID")]
+        public string PID { get; set; }
+        [Display(Name = "Full Name*")]
+        [Required(ErrorMessage = "Full Name is required")]
+        public string PName { get; set; }
+        [Display(Name = "Contact No*")]
+        [Required(ErrorMessage = "Contact No is required")]
+        public string Mobile { get; set; }
+        [Display(Name = "Email")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "E-mail is not valid")]
+        public string Mail { get; set; }
+        [Display(Name = "Designation")]
+        public string Designation { get; set; }
+        [Display(Name = "Department")]
+        public string Department { get; set; }
+        public Nullable<System.Guid> DepartmentKey { get; set; }
+        public byte[] Pic { get; set; }
+        public string PicType { get; set; }
+        public Nullable<System.Guid> CompanyKey { get; set; }
+        [Required(ErrorMessage = "Usergroup is not selected Correctly")]
+        public Nullable<System.Guid> Usergr { get; set; }
+        [Display(Name = "Username*")]
+        [Required(ErrorMessage = "Username is required")]
+        public string Username { get; set; }
+        [Display(Name = "Password*")]
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+        [Display(Name = "Confirm Password*")]
+        [Required(ErrorMessage = "Please enter the Password again.")]
+        [Compare("Password", ErrorMessage = "Your Confirm Password does not match.")]
+        public string ConfirmPassword { get; set; }
+        public Nullable<bool> IsUser { get; set; }
+        public virtual Usergroup Usergroup { get; set; }
+    }
+}
---
+++ Jamiahsalafiyah.Web/Models/UniqueIDCreator.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Jamiahsalafiyah.Web.Models
+{
+    public class UniqueIDCreator
+    {
+        public static string NumberToWords(int number)
+        {
+            if (number == 0)
+                return "zero";
+
+            if (number < 0)
+                return "minus " + NumberToWords(Math.Abs(number));
+
+            string words = "";
+
+            if ((number / 1000000) > 0)
+            {
+                words += NumberToWords(number / 1000000) + " Million ";
+                number %= 1000000;
+            }
+
+            if ((number / 1000) > 0)
+            {
+                words += NumberToWords(number / 1000) + " Thousand ";
+                number %= 1000;
+            }
+
+            if ((number / 100) > 0)
+            {
+                words += NumberToWords(number / 100) + " Hundred ";
+                number %= 100;
+            }
+
+            if (number > 0)
+            {
+                if (words != "")
+                    words += "and ";
+
+                var unitsMap = new[] { "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
+                var tensMap = new[] { "Zero", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
+
+                if (number < 20)
+                    words += unitsMap[number];
+                else
+                {
+                    words += tensMap[number / 10];
+                    if ((number % 10) > 0)
+                        words += "-" + unitsMap[number % 10];
+                }
+            }
+
+            return words;
+        }
+
+
+
+
+        public static string CreateID()
+        {
+            long numIterations = 0;
+            do
+            {
+
+                Random rand = new Random((int)DateTime.Now.Ticks);
+                numIterations = rand.Next(100, 999999999);
+
+            } while (CheckStaffID(numIterations.ToString()));
+
+            return numIterations.ToString();
+        }
+        private static bool CheckStaffID(string id)
+        {
+            JAMAIAHSALAFIYAH_HOUSTONEntities db = new JAMAIAHSALAFIYAH_HOUSTONEntities();
+            var uid = from x in db.StaffList where x.PID == id && x.CompanyKey == GlobalClass.Company.CompanyKey select x;
+            if (uid.Count() > 0) return true;
+            else return false;
+        }
+        public static string UniqueIDFromDateTimeOnly()
+        {
+            DateTime date = DateTime.Now;
+
+            string uniqueID = String.Format(
+              "{0:0000}{1:00}{2:00}{3:00}{4:00}{5:00}",
+              date.Year, date.Month, date.Day,
+              date.Hour, date.Minute, date.Second
+              );
+            return uniqueID;
+
+        }
+        public static string UniqueCode()
+        {
+            string characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            string ticks = DateTime.UtcNow.Ticks.ToString();
+            var code = "";
+            for (var i = 0; i < 6; i += 2)
+            {
+                if ((i + 2) <= ticks.Length)
+                {
+                    var number = int.Parse(ticks.Substring(i, 2));
+                    if (number > characters.Length - 1)
+                    {
+                        var one = double.Parse(number.ToString().Substring(0, 1));
+                        var two = double.Parse(number.ToString().Substring(1, 1));
+                        code += characters[Convert.ToInt32(one)];
+                        code += characters[Convert.ToInt32(two)];
+                    }
+                    else
+                        code += characters[number];
+                }
+            }
+            return code;
+        }
+
+        public static int BusinessDaysUntil(DateTime firstDay, DateTime lastDay)//, params DateTime[] bankHolidays)
+        {
+            firstDay = firstDay.Date;
+            lastDay = lastDay.Date;
+            if (firstDay > lastDay)
+                throw new ArgumentException("Incorrect last day " + lastDay);
+
+            TimeSpan span = lastDay - firstDay;
+            int businessDays = span.Days + 1;
+            int fullWeekCount = businessDays / 7;
+            // find out if there are weekends during the time exceedng the full weeks
+            if (businessDays > fullWeekCount * 7)
+            {
+                // we are here to find out if there is a 1-day or 2-days weekend
+                // in the time interval remaining after subtracting the complete weeks
+                int firstDayOfWeek = (int)firstDay.DayOfWeek;
+                int lastDayOfWeek = (int)lastDay.DayOfWeek;
+                if (lastDayOfWeek < firstDayOfWeek)
+                    lastDayOfWeek += 7;
+                if (firstDayOfWeek <= 6)
+                {
+                    if (lastDayOfWeek >= 7)// Both Saturday and Sunday are in the remaining time interval
+                        businessDays -= 2;
+                    else if (lastDayOfWeek >= 6)// Only Saturday is in the remaining time interval
+                        businessDays -= 1;
+                }
+                else if (firstDayOfWeek <= 7 && lastDayOfWeek >= 7)// Only Sunday is in the remaining time interval
+                    businessDays -= 1;
+            }
+
+            // subtract the weekends during the full weeks in the interval
+            businessDays -= fullWeekCount + fullWeekCount;
+
+            // subtract the number of bank holidays during the time interval
+            //foreach (DateTime bankHoliday in bankHolidays)
+            //{
+            //    DateTime bh = bankHoliday.Date;
+            //    if (firstDay <= bh && bh <= lastDay)
+            //        --businessDays;
+            //}
+
+            return businessDays;
+        }
+    }
+}
